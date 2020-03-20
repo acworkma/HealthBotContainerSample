96953ce1f59d7ffd1cc6522f8e17b0ff10a4d3e1
Adam Workman <adam@Taz.local>
2020-03-20 16:19:32 -0600

    learn how to spell assessment

--- public/index.js
+++ public/index.js
@@ -83,7 +83,7 @@ function initBotConversation() {
     botConnection.postActivity({
         type: "invoke",
         value: {
-            trigger: "covid19"
+            trigger: "covid19_assessment"
         },
         from: user,
         name: "TriggerScenario"
