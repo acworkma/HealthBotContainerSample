59974bfcb4f7eebcb4a2fe300bddfbd1584c79b3
Adam Workman <adam@Taz.local>
2020-04-03 16:58:03 -0600

    typo - comma

--- public/index.js
+++ public/index.js
@@ -89,7 +89,7 @@ function initBotConversation() {
         hideSendBox: true , /* set to true to hide the send box from the view */
         //botAvatarInitials: 'Bot',
         //userAvatarInitials: 'You',
-        backgroundColor: '#F8F8F8',
+        backgroundColor: '#F8F8F8'
     };
 
     const store = window.WebChat.createStore({}, function(store) { return function(next) { return function(action) {
